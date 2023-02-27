ec33f4126c3f5d91c476ca8d0e96fd4ae7f9e28e
juliarose <julialunarose@gmail.com>
2023-02-27 05:01:45 -0500

    this doesn't need tf2-enum in deps

--- src/response/dashboard/item.rs
+++ src/response/dashboard/item.rs
@@ -1,6 +1,6 @@
 use serde::{Serialize, Deserialize};
 use crate::SKU;
-use crate::tf2_enum::Quality;
+use tf2_sku::tf2_enum::Quality;
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Item {
